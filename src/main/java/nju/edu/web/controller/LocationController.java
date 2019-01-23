8c735cf5ce47602ab34a35c23e49eba040f9f990
kukubao207 <502914814@qq.com>
2019-01-23 20:15:53 +0800

    citylist

--- src/main/java/nju/edu/web/controller/LocationController.java
+++ src/main/java/nju/edu/web/controller/LocationController.java
@@ -1,16 +1,12 @@
 package nju.edu.web.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import nju.edu.service.CityService;
-import nju.edu.util.HttpClientUtil;
 import nju.edu.util.LocationUtil;
-import nju.edu.util.ResponseCode;
 import nju.edu.web.vo.ResultData;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -22,19 +18,19 @@ public class LocationController {
     private CityService cityService;
 
     //根据ip解析地址
-    @RequestMapping(method = RequestMethod.POST, value = "/ip/resolve")
+    @PostMapping("/ip/resolve")
     public ResultData ip2address(String ip) {
         return LocationUtil.ip2address(ip);
     }
 
     //根据地址解析经纬度
-    @RequestMapping(method = RequestMethod.POST, value = "/address/resolve")
+    @PostMapping("/address/resolve")
     public ResultData geocoder(String address) {
         return LocationUtil.geocoder(address);
     }
 
     //获取城市列表
-    @RequestMapping(method = RequestMethod.GET, value = "/citylist")
+    @GetMapping("city/list")
     public ResultData getCityList(){
         return cityService.getCityList();
     }
